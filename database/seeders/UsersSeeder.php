71b04033fe8475146bcedbbd38b9a807913c9cb6
Code with Luis <codewithluis11@gmail.com>
2022-03-11 11:13:10 -0600

    Add Laravel Breeze, add UsersSeeder

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('password')
+            ]
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
